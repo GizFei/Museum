019cc7a5bd8396a4af3d0a123ced21a3c1456e73
LXH-TK <tk@zju.edu.cn>
2018-10-12 23:07:42 +0800

    update

--- app/src/main/java/com/giz/museum/MuseumMapActivity.java
+++ app/src/main/java/com/giz/museum/MuseumMapActivity.java
@@ -1,17 +1,28 @@
 package com.giz.museum;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocation;
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.MyLocationStyle;
 
 public class MuseumMapActivity extends AppCompatActivity {
 
     private MapView mMapView;
+    // 声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    public AMapLocationListener mLocationListener;
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption = null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,11 +32,46 @@ public class MuseumMapActivity extends AppCompatActivity {
         mMapView = findViewById(R.id.the_map);
         mMapView.onCreate(savedInstanceState);
         AMap aMap = mMapView.getMap();
+
+        // 初始化定位
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mLocationListener);
+        //设置定位模式为高精度模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置定位间隔，单位毫秒，默认为2000ms，最低1000ms。
+        mLocationOption.setInterval(1000);
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mLocationClient.startLocation();
+
+        // 显示定位蓝点
+        MyLocationStyle myLocationStyle;
+        myLocationStyle = new MyLocationStyle();
+        // 连续定位、蓝点不会移动到地图中心点，定位点依照设备方向旋转，并且蓝点会跟随设备移动
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER);
+        // 设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒
+        myLocationStyle.interval(1000);
+        // 蓝点精确度个性化
+        myLocationStyle.strokeWidth(0);
+        // 设置定位蓝点的Style
+        aMap.setMyLocationStyle(myLocationStyle);
+        // 设置默认定位按钮是否显示，即回到小蓝点
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);
+        // 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false
+        aMap.setMyLocationEnabled(true);
+        // 是否显示定位蓝点
+        myLocationStyle.showMyLocation(true);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        //停止定位后，本地定位服务并不会被销毁
+        mLocationClient.stopLocation();
         mMapView.onDestroy();
     }
 
