cb3f8d16045cc08cde03fd6e304bdac1eb5ce6db
李新豪 <lixinhao@TKdeMacBook-Air.local>
2018-10-13 09:50:27 +0800

    commit 10.13-1

--- app/src/main/java/com/giz/museum/InfoFragment.java
+++ app/src/main/java/com/giz/museum/InfoFragment.java
@@ -3,22 +3,13 @@ package com.giz.museum;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
-import com.amap.api.maps.AMap;
-import com.amap.api.maps.MapView;
-import com.giz.utils.Museum;
-import com.giz.utils.MuseumLib;
-import com.giz.utils.MuseumPicturePagerAdapter;
-
-import java.util.UUID;
+import com.giz.bmob.Museum;
+import com.giz.customize.MuseumLib;
 
 public class InfoFragment extends Fragment {
 
@@ -32,7 +23,7 @@ public class InfoFragment extends Fragment {
      * @param museumId 博物馆ID
      * @return InfoFragment实例
      */
-    public static InfoFragment newInstance(UUID museumId){
+    public static InfoFragment newInstance(String museumId){
         InfoFragment fragment = new InfoFragment();
         Bundle bundle = new Bundle();
 
@@ -45,7 +36,7 @@ public class InfoFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        UUID uuid = (UUID)getArguments().getSerializable(ARGS_ID);
+        String uuid = getArguments().getString(ARGS_ID);
 //        Log.d(TAG, uuid.toString());
 
         mMuseum = MuseumLib.get(getContext()).getMuseumById(uuid);
